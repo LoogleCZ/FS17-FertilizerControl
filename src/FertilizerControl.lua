5096732994ea569f31029d743ef82f6b25928dcd
Martin Fabík <mar.fabik@gmail.com>
2018-01-18 19:04:16 +0100

    UX of consumption indication
    UX of indication - user can turn off indication and status will be saved.

--- src/FertilizerControl.lua
+++ src/FertilizerControl.lua
@@ -7,7 +7,7 @@
 -- Free for non-comerecial usage!
 --
 -- version ID   - 1.0.0
--- version date - 2018-01-17 19:58
+-- version date - 2018-01-18 19:00
 --
 -- used namespace: LFC
 --
@@ -25,22 +25,39 @@ end;
 function FertilizerControl:load(savegame)
 	self.LFC = {};
 	self.LFC.fConsumption = 0;
-	--
-	-- loaded from XML:
-	-- if display consumption
-	-- from wchich spped display consumption
-	-- if width is fixed num or calculated dynamically.
-	--
+	self.LFC.widthCalculationDynamic = (Utils.getNoNil(getXMLString(self.xmlFile, "vehicle.fertilizerControl.consumptionIndicator#widthCalculation"), "dynamic") == "dynamic");
+	if self.LFC.widthCalculationDynamic then
+		self.LFC.width = 0;
+	else
+		self.LFC.width = self.sprayUsageScale.workingWidth;
+	end;
+	self.LFC.minimumDisplaySpeed = Utils.getNoNil(getXMLInt(self.xmlFile,	"vehicle.fertilizerControl.consumptionIndicator#minimumDisplaySpeed"), 4);
+	self.LFC.indicatorAllowed = Utils.getNoNil(getXMLBool(self.xmlFile,	"vehicle.fertilizerControl.consumptionIndicator#active"), true);
+	self.LFC.indicatorActived = Utils.getNoNil(getXMLBool(self.xmlFile,	"vehicle.fertilizerControl.consumptionIndicator#defaultActive"), true);
 end;
 
 function FertilizerControl:postLoad(savegame)
+	self.LFC.indicatorActived = Utils.getNoNil(getXMLBool(savegame.xmlFile, savegame.key ..	"#showFertilizerConsumption"), self.LFC.indicatorActived);
 end;
 
+function FertilizerControl:getSaveAttributesAndNodes(nodeIdent)
+	attributes= " showFertilizerConsumption=\"" .. tostring(self.LFC.indicatorActived) .. "\"";
+    nodes = "";
+    return attributes, nodes;
+end
+
 function FertilizerControl:delete() end;
-function FertilizerControl:loadFromAttributesAndNodes(xmlFile, key, resetVehicles) end;
 
 function FertilizerControl:update(dt)
-	
+	if self.isClient then
+		if InputBinding.isPressed(InputBinding.lfc_consumptionSetup) then
+			if self.LFC.indicatorAllowed then
+				if InputBinding.hasEvent(InputBinding.lfc_showConsumption) then
+					self.LFC.indicatorActived = not self.LFC.indicatorActived;
+				end;
+			end;
+		end;
+	end;
 end;
 
 function FertilizerControl:updateTick(dt)
@@ -53,53 +70,44 @@ function FertilizerControl:updateTick(dt)
 		-- By changing default scale we can increase or decrease consumption
 		-- Must change for each filltype 
 		--
-		print("==== DEBUG ====");
-		print("Speed (km/h): " .. tostring(self.lastSpeed*3600));
-		print("Speed (m/s): " .. tostring((self.lastSpeed*1000)));
-		print("Attacher vehicle: " .. tostring(self.attacherVehicle));
-		print("GetIsActive: " .. tostring(self:getIsActive()));
-		print("GetIsTurnedOn: " .. tostring(self:getIsTurnedOn()));
-		if self.attacherVehicle ~= nil
-			and self:getIsActive()
-			and self:getIsTurnedOn()
-			and (self.lastSpeed*3600) > 4 then
-			local fillType = self:getUnitLastValidFillType(self.sprayer.fillUnitIndex);
-			if fillType == FillUtil.FILLTYPE_UNKNOWN and self.fillUnits[self.sprayer.fillUnitIndex] ~= nil then
-				for unitFillType,state in pairs(self.fillUnits[self.sprayer.fillUnitIndex].fillTypes) do
-					if unitFillType ~= FillUtil.FILLTYPE_UNKNOWN and state then
-						fillType = unitFillType;
-						break;
+		if self.LFC.indicatorAllowed and self.LFC.indicatorActived then
+			print("==== DEBUG ====");
+			print("Speed (km/h): " .. tostring(self.lastSpeed*3600));
+			print("Speed (m/s): " .. tostring((self.lastSpeed*1000)));
+			print("Attacher vehicle: " .. tostring(self.attacherVehicle));
+			print("GetIsActive: " .. tostring(self:getIsActive()));
+			print("GetIsTurnedOn: " .. tostring(self:getIsTurnedOn()));
+			if self.attacherVehicle ~= nil
+				and self:getIsActive()
+				and self:getIsTurnedOn()
+				and (self.lastSpeed*3600) > self.LFC.minimumDisplaySpeed then
+				local fillType = self:getUnitLastValidFillType(self.sprayer.fillUnitIndex);
+				if fillType == FillUtil.FILLTYPE_UNKNOWN and self.fillUnits[self.sprayer.fillUnitIndex] ~= nil then
+					for unitFillType,state in pairs(self.fillUnits[self.sprayer.fillUnitIndex].fillTypes) do
+						if unitFillType ~= FillUtil.FILLTYPE_UNKNOWN and state then
+							fillType = unitFillType;
+							break;
+						end
 					end
 				end
-			end
-			local litersPerSecond = self:getLitersPerSecond(fillType);
-			print("Liters per second: " .. tostring(litersPerSecond));
-			
-			print("Test type: " .. tostring(WorkArea.AREATYPE_SPRAYER));
-			local areas = self.workAreaByType[WorkArea.AREATYPE_SPRAYER]
-			print("Test type table: " .. tostring(areas));
-			print("Areas count: " .. tostring(#areas));
-			width = 0;
-			for _, workArea in pairs(areas) do
-				if self:getIsWorkAreaActive(workArea) then
-					local x,_,z = getWorldTranslation(workArea.start);
-					local x1,_,z1 = getWorldTranslation(workArea.width);
-					width = width + math.sqrt((x1 - x)*(x1 - x) + (z1 - z)*(z1 - z));
+				local litersPerSecond = self:getLitersPerSecond(fillType);
+				
+				width = self.LFC.width;
+				if self.LFC.widthCalculationDynamic then
+					local areas = self.workAreaByType[WorkArea.AREATYPE_SPRAYER];
+					for _, workArea in pairs(areas) do
+						if self:getIsWorkAreaActive(workArea) then
+							local x,_,z = getWorldTranslation(workArea.start);
+							local x1,_,z1 = getWorldTranslation(workArea.width);
+							width = width + math.sqrt((x1 - x)*(x1 - x) + (z1 - z)*(z1 - z));
+						end;
+					end;
 				end;
+				
+				self.LFC.fConsumption = (litersPerSecond/(width*self.lastSpeed))*10;
 			end;
-			print("test width: " .. tostring(width));		
-			-- (l/s)/(m/s) = l/m
-			-- (l/m) * (100*100)
-			-- 100 * 100 is one Ha
-			self.LFC.fConsumption = (litersPerSecond/(width*self.lastSpeed))*10;
-			
-			--
-			--	Test:
-			--
-			
-			self.sprayUsageScale.default = self.sprayUsageScale.default + 0.1;
+			print("===============");
 		end;
-		print("===============");
 	end;
 end;
 
@@ -110,11 +118,27 @@ function FertilizerControl:keyEvent(unicode, sym, modifier, isDown) end;
 
 function FertilizerControl:draw()
 	if self.isClient then
-		if self.attacherVehicle ~= nil
-			and self:getIsActive()
-			and self:getIsTurnedOn()
-			and (self.lastSpeed*3600) > 4 then
-			g_currentMission:addExtraPrintText(string.format(g_i18n:getText("lfc_consumptionIndicator"), self.LFC.fConsumption));
+		g_currentMission:addHelpButtonText(g_i18n:getText("lfc_consumptionSetup"), InputBinding.lfc_consumptionSetup);
+		if InputBinding.isPressed(InputBinding.lfc_consumptionSetup) then
+			if self.LFC.indicatorAllowed then
+				if self.LFC.indicatorActived then
+					g_currentMission:addHelpButtonText(g_i18n:getText("lfc_showConsumption_h"), InputBinding.lfc_showConsumption);
+				else
+					g_currentMission:addHelpButtonText(g_i18n:getText("lfc_showConsumption_s"), InputBinding.lfc_showConsumption);
+				end;
+			end;
+			g_currentMission:addHelpButtonText(g_i18n:getText("lfc_consumptionUp"), InputBinding.lfc_consumptionUp);
+			g_currentMission:addHelpButtonText(g_i18n:getText("lfc_consumptionDefault"), InputBinding.lfc_consumptionDefault);
+			g_currentMission:addHelpButtonText(g_i18n:getText("lfc_consumptionDown"), InputBinding.lfc_consumptionDown);
+		end;
+	
+		if self.LFC.indicatorAllowed and self.LFC.indicatorActived then
+			if self.attacherVehicle ~= nil
+				and self:getIsActive()
+				and self:getIsTurnedOn()
+				and (self.lastSpeed*3600) > self.LFC.minimumDisplaySpeed then
+				g_currentMission:addExtraPrintText(string.format(g_i18n:getText("lfc_consumptionIndicator"), self.LFC.fConsumption));
+			end;
 		end;
 	end;
 end;
